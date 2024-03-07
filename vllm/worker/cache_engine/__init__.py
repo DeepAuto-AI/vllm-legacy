fdc7f7b98d1515a02830abce0f0164028a937ec8
AinL <gmlwns5176@gmail.com>
2024-03-07 21:55:18 +0900

    update

--- vllm/worker/cache_engine/__init__.py
+++ vllm/worker/cache_engine/__init__.py
@@ -1,1 +1,10 @@
-from .cache_engine import CacheConfig, CacheEngine
+from .cache_engine import CacheConfig
+from .cache_engine import CacheEngine as VllmCacheEngine
+from .hip_cache_engine import HipCacheEngine
+import os
+
+CacheEngine = VllmCacheEngine
+# if 'vllm' in [os.getenv('PAGED_ATTENTION_METHOD', 'timber'), os.getenv('PROMPT_ATTENTION_METHOD', 'timber')]:
+#     CacheEngine = VllmCacheEngine
+# else:
+#     CacheEngine = HipCacheEngine
