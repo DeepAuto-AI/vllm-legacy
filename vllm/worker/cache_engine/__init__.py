313bab259172057702eeb9bdf6ddafb3c7bf4d36
AinL <gmlwns5176@gmail.com>
2024-03-08 20:05:48 +0900

    upudate

--- vllm/worker/cache_engine/__init__.py
+++ vllm/worker/cache_engine/__init__.py
@@ -1,9 +1,13 @@
 from .cache_engine import CacheConfig
+
 from .cache_engine import CacheEngine as VllmCacheEngine
 from .hip_cache_engine import HipCacheEngine
+from .map_cache_engine import MapCacheEngine
+
 import os
 
 CacheEngine = VllmCacheEngine
+# CacheEngine = MapCacheEngine
 # if 'vllm' in [os.getenv('PAGED_ATTENTION_METHOD', 'timber'), os.getenv('PROMPT_ATTENTION_METHOD', 'timber')]:
 #     CacheEngine = VllmCacheEngine
 # else:
